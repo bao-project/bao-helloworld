202b463644c18df07c47ef970498ca797080b59e
Diogo21Costa <diogoandreveigacosta@gmail.com>
2023-11-10 18:12:23 +0000

    feat(risc-v): include changes to run bare+freeRTOS setup on qemu-riscv64
    Signed-off-by: Diogo21Costa <diogoandreveigacosta@gmail.com>

--- configs/baremetal-freeRTOS.c
+++ configs/baremetal-freeRTOS.c
@@ -1,83 +1,66 @@
 #include <config.h>
-
 VM_IMAGE(baremetal_image, XSTR(BAO_WRKDIR_IMGS/baremetal-freeRTOS-setup/baremetal.bin));
-VM_IMAGE(freertos_image, XSTR(BAO_WRKDIR_IMGS/baremetal-freeRTOS-setup/free-rtos.bin));
+VM_IMAGE(freertos_image, XSTR(BAO_WRKDIR_IMGS/baremetal-freeRTOS-setup/freertos.bin));
 
 struct config config = {
-    
+
     CONFIG_HEADER
-    
+
     .shmemlist_size = 1,
     .shmemlist = (struct shmem[]) {
         [0] = { .size = 0x00010000, }
     },
-    
+
     .vmlist_size = 2,
     .vmlist = {
-        { 
+        {
             .image = {
-                .base_addr = 0x50000000,
+                .base_addr = 0x80200000,
                 .load_addr = VM_IMAGE_OFFSET(baremetal_image),
                 .size = VM_IMAGE_SIZE(baremetal_image)
             },
-
-            .entry = 0x50000000,
-
+            .entry = 0x80200000,
             .platform = {
                 .cpu_num = 3,
-                
                 .region_num = 1,
                 .regions =  (struct vm_mem_region[]) {
                     {
-                        .base = 0x50000000,
-                        .size = 0x4000000 
+                        .base = 0x80200000,
+                        .size = 0x4000000
                     }
                 },
-
-                .dev_num = 2,
+                .dev_num = 1,
                 .devs =  (struct vm_dev_region[]) {
-                    {   
+                    {
                         /* PL011 */
-                        .pa = 0x9000000,
-                        .va = 0x9000000,
-                        .size = 0x10000,                     
-                    },
-                    {   
-                        /* Arch timer interrupt */
+                        .pa = 0x10000000,
+                        .va = 0x10000000,
+                        .size = 0x10000,
                         .interrupt_num = 1,
-                        .interrupts = 
-                            (irqid_t[]) {27}                         
+                        .interrupts = (irqid_t[]) {10}
                     }
                 },
-
                 .arch = {
-                    .gic = {
-                        .gicd_addr = 0x08000000,
-                        .gicr_addr = 0x080A0000,
-                    }
+                    .plic_base = 0xc000000,
                 }
             },
         },
-        { 
+        {
             .image = {
                 .base_addr = 0x0,
                 .load_addr = VM_IMAGE_OFFSET(freertos_image),
                 .size = VM_IMAGE_SIZE(freertos_image)
             },
-
             .entry = 0x0,
-
             .platform = {
                 .cpu_num = 1,
-                
                 .region_num = 1,
                 .regions =  (struct vm_mem_region[]) {
                     {
                         .base = 0x0,
-                        .size = 0x8000000 
+                        .size = 0x8000000
                     }
                 },
-
                 .ipc_num = 1,
                 .ipcs = (struct ipc[]) {
                     {
@@ -88,26 +71,17 @@ struct config config = {
                         .interrupts = (irqid_t[]) {52}
                     }
                 },
-
-                .dev_num = 2,
+                .dev_num = 1,
                 .devs =  (struct vm_dev_region[]) {
-                    {   
+                    {
                         /* PL011 */
-                        .pa = 0x9000000,
+                        .pa = 0x10000000,
                         .va = 0xff000000,
-                        .size = 0x10000                        
-                    },
-                    {   
-                        .interrupt_num = 1,
-                        .interrupts = (irqid_t[]) {27}                         
+                        .size = 0x10000
                     }
                },
-
                 .arch = {
-                    .gic = {
-                        .gicd_addr = 0xf9010000,
-                        .gicr_addr = 0xf9020000,
-                    }
+                    .plic_base = 0xc000000,
                 }
             },
         },
