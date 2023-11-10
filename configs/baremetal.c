65e598d50ac28c3937454faee7265d2881c50d5b
Diogo21Costa <diogoandreveigacosta@gmail.com>
2023-11-10 17:19:50 +0000

    feat(risc-v): include changes to run baremetal setup on qemu-riscv64
    Signed-off-by: Diogo21Costa <diogoandreveigacosta@gmail.com>

--- configs/baremetal.c
+++ configs/baremetal.c
@@ -3,54 +3,44 @@
 VM_IMAGE(baremetal_image, XSTR(BAO_WRKDIR_IMGS/baremetal-setup/baremetal.bin));
 
 struct config config = {
-    
     CONFIG_HEADER
-    
     .vmlist_size = 1,
     .vmlist = {
-        { 
+        {
             .image = {
-                .base_addr = 0x50000000,
+                .base_addr = 0x80200000,
                 .load_addr = VM_IMAGE_OFFSET(baremetal_image),
                 .size = VM_IMAGE_SIZE(baremetal_image)
             },
-
-            .entry = 0x50000000,
-
+            .entry = 0x80200000,
             .platform = {
                 .cpu_num = 4,
-                
                 .region_num = 1,
                 .regions =  (struct vm_mem_region[]) {
                     {
-                        .base = 0x50000000,
-                        .size = 0x4000000 
+                        .base = 0x80200000,
+                        .size = 0x4000000
                     }
                 },
-
                 .dev_num = 2,
                 .devs =  (struct vm_dev_region[]) {
-                    {   
+                    {
                         /* PL011 */
-                        .pa = 0x9000000,
-                        .va = 0x9000000,
+                        .pa = 0x10000000,
+                        .va = 0x10000000,
                         .size = 0x10000,
                         .interrupt_num = 1,
-                        .interrupts = (irqid_t[]) {33}                        
+                        .interrupts = (irqid_t[]) {10}
                     },
-                    {   
+                    {
                         /* Arch timer interrupt */
                         .interrupt_num = 1,
-                        .interrupts = 
-                            (irqid_t[]) {27}                         
+                        .interrupts =
+                            (irqid_t[]) {27}
                     }
                 },
-
                 .arch = {
-                    .gic = {
-                        .gicd_addr = 0x08000000,
-                        .gicr_addr = 0x080A0000,
-                    }
+                    .plic_base = 0xc000000,
                 }
             },
         }
